ddedf9424648afb54b46ca1c8b3d51b9704cdb82
Jonathan Couldridge <jcouldridge@outlook.com>
2017-10-15 14:34:04 +0100

    x7, x9 tests

---
+++ test/ill8.Cpu.Test/InstructionsTests/7XNNTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using Xunit;
+
+namespace ill8.Cpu.Test.InstructionsTests
+{
+    public class _7XNNTests
+    {
+        private readonly Instructions _sut;
+
+        public _7XNNTests()
+        {
+            var cpu = new Mock<ICpu>()
+                .SetupAllProperties()
+                .SetupProperty(x => x.V, new byte[16])
+                .Object;
+            _sut = new Instructions(cpu);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(15)]
+        public void IncrementVxByNN(byte x)
+        {
+            _sut.Cpu.V[x] = 0; //ensure we're initialised to 0
+
+            ushort opcode = (ushort)(0x70ea | x << 8);
+
+            _sut._7XNN(opcode);
+
+            Assert.Equal(0xea, _sut.Cpu.V[x]);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(15)]
+        public void WrapAroundIfIncrementTakesBeyondFF(byte x)
+        {
+            _sut.Cpu.V[x] = 2; //ensure we're initialised to something greater than 0
+
+            ushort opcode = (ushort)(0x70ff | x << 8);
+
+            _sut._7XNN(opcode);
+
+            Assert.Equal((2 + 0xff) & 0xff, _sut.Cpu.V[x]);
+        }
+    }
+}
---
+++ test/ill8.Cpu.Test/InstructionsTests/9XY0Tests.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using Xunit;
+
+namespace ill8.Cpu.Test.InstructionsTests
+{
+    public class _9XY0Tests
+    {
+        private readonly Instructions _sut;
+
+        public _9XY0Tests()
+        {
+            var cpu = new Mock<ICpu>()
+                .SetupAllProperties()
+                .SetupProperty(x => x.V, new byte[16])
+                .Object;
+            _sut = new Instructions(cpu);
+        }
+
+        [Theory]
+        [InlineData(15, 0)]
+        [InlineData(12, 7)]
+        public void IncreasePCBy2IfConditionMet(byte x, byte y)
+        {
+            var pc = _sut.Cpu.PC;
+
+            _sut.Cpu.V[x] = 0xab;
+            _sut.Cpu.V[y] = 0xea;
+            ushort opcode = (ushort)(0x5000 | x << 8 | y << 4);
+
+            _sut._9XY0(opcode);
+
+            Assert.Equal(pc+2, _sut.Cpu.PC);
+        }
+
+        [Theory]
+        [InlineData(0, 15)]
+        [InlineData(12, 7)]
+        public void LeavePCAloneIfConditionNotMet(byte x, byte y)
+        {
+            var pc = _sut.Cpu.PC;
+
+            _sut.Cpu.V[x] = 0xab;
+            _sut.Cpu.V[y] = 0xab;
+            ushort opcode = (ushort)(0x5000 | x << 8 | y << 4);
+
+            _sut._9XY0(opcode);
+
+            Assert.Equal(pc, _sut.Cpu.PC);
+        }
+    }
+}
